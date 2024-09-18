f904bb1af8db941950c30e07852805ef7b7847d1
prejinaa <prejina.191629@ncit.edu.np>
2024-09-18 17:04:44 +0545

    test commit

--- src/main/java/com/exampl/merchant/MerchantApplication.java
+++ src/main/java/com/exampl/merchant/MerchantApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class MerchantApplication {
 
+//
 	public static void main(String[] args) {
 		SpringApplication.run(MerchantApplication.class, args);
 	}
